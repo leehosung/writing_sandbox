8f29e9250a1223d23cf2f5ada07e808b73f166b2
Lee Ho Sung <lee.ho.sung@gmail.com>
2014-11-05 14:58:31 +0900

    Basic quiz view now returns minimal HTML

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from quiz.views import home_page
 
 
@@ -9,3 +11,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        content = response.content
+        self.assertTrue(content.startswith(b'<html>'))
+        self.assertIn(b'<title>Writing Sandbox</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
