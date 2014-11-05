9672bbc4aac0cadfc3c7183a8628b9929ee34cbe
lee ho sung <lee.ho.sung@gmail.com>
2014-11-05 23:50:25 +0900

    now we can use admin page

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from quiz.models import Phrase
+from quiz.models import PlayerRecord
 
-# Register your models here.
+
+class PhraseAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlayerRecordAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Phrase, PhraseAdmin)
+admin.site.register(PlayerRecord, PlayerRecordAdmin)
