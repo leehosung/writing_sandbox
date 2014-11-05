08beda82ca89132289b4fae340ae6dac3cc0e63c
Lee Ho Sung <lee.ho.sung@gmail.com>
2014-11-05 16:37:36 +0900

    add an input functional test on a user side

--- functional_tests/functional_tests_user.py
+++ functional_tests/functional_tests_user.py
@@ -15,9 +15,10 @@ class UserTest(unittest.TestCase):
         # He goes to check out its homepage
         self.browser.get('http://localhost:5000')
 
-        # He notices the page title and header mention "writing sandbox"
+        # He notices the page title and header mention "Writing Sandbox"
         self.assertIn('Writing Sandbox', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Writing Sandbox', header_text)
 
         # He logs into the service. 
 
@@ -25,7 +26,20 @@ class UserTest(unittest.TestCase):
 
         # He sees a sentence in Korean
 
-        # He writes a sentence in English 
+        # He can see a text box
+        inputbox = self.browser.find_element_by_id('id_input_english')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Write an English sentense'
+        )
+
+        # He types "I have a question" into a text box
+        inputbox.send_keys('I have question')
+
+        # When he hits enter, the pages updates, and now the page shows
+        # "I have a question" as a right answer
+        inputbox.send_keys(Keys.ENTER)
+        self.fail('Finish the test!')
 
         # He types enters, and a sentence which Admin has registered in advance will show up
 
