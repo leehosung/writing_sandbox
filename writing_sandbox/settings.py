bb5659819c19df3cec664ead409b0de28bddd391
Lee Ho Sung <lee.ho.sung@gmail.com>
2014-11-07 09:59:29 +0900

    using postgresql only in Heroku

--- writing_sandbox/settings.py
+++ writing_sandbox/settings.py
@@ -68,9 +68,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES= {'default': dj_database_url.config()}
+DATABASES = { 'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+              }
+            }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
