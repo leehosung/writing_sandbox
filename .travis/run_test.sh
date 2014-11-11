f1c0a9d1c149f91c27005a862ffb089f2f58a9c0
Lee Ho Sung <lee.ho.sung@gmail.com>
2014-11-11 19:00:47 +0900

    change condition after reading http://docs.travis-ci.com/user/ci-environment/

--- .travis/run_test.sh
+++ .travis/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $TRAVIS_BRANCH == 'master' && ! $TRAVIS_PULL_REQUEST ]]; then
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   python3 manage.py test 
 else
   python3 manage.py test quiz
