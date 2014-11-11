09f912b60b383df16b30a20125fef8228477b402
Lee Ho Sung <lee.ho.sung@gmail.com>
2014-11-11 12:22:09 +0900

    remove duplicated part

--- functional_tests/test_admin.py
+++ functional_tests/test_admin.py
@@ -1,50 +1,22 @@
-import os
 from django.test import LiveServerTestCase
-from django.conf import settings
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-
-chromedriver = '%s/chromedriver' % os.path.dirname(os.path.abspath(__file__))
-if os.path.isfile(chromedriver):
-    os.environ['webdriver.chrome.driver'] = chromedriver
 
 
 class AdminTest(LiveServerTestCase):
 
     def __init__(self, *args, **kwargs):
         super(AdminTest, self).__init__(*args, **kwargs)
-        #if settings.DEBUG == False:
-        #    settings.DEBUG = True
 
     def setUp(self):
-        # TODO : DRY
-        if 'TRAVIS' in os.environ:
-            capabilities = dict()
-            capabilities["build"] = os.environ["TRAVIS_BUILD_NUMBER"]
-            capabilities["tags"] = [os.environ["TRAVIS_PYTHON_VERSION"], "CI"]
-            USERNAME = os.environ.get('SAUCE_USERNAME')
-            ACCESS_KEY = os.environ.get('SAUCE_ACCESS_KEY')
-            sauce_url = "http://%s:%s@localhost:4445"
-            self.brower = webdriver.Remote(desired_capabilities=capabilities, command_executor=sauce_url % (USERNAME, ACCESS_KEY))
-            self.browser.implicitly_wait(30)
-        else:
-            try:
-                self.browser = webdriver.Chrome(chromedriver)
-            except WebDriverException:
-                self.browser = webdriver.Firefox()
-            self.browser.implicitly_wait(3)
+        pass
 
     def tearDown(self):
-        self.browser.quit()
+        pass
 
     def test_can_see_a_list(self):
         # Nasol is the admin of the Writing Sandbox
         # She goes to manage this service
-        self.browser.get(self.live_server_url + "/admin")
 
         # She notices the page title and header mention "page for admin"
-        self.assertIn('admin', self.browser.title)
 
         # She chooses a category to register sentences
 
